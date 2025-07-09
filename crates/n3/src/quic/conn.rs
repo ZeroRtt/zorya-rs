31c7a500d7ebbe1d01fe55bfc504468f6a9bf0b2
yayanyang <yayanyang@gmail.com>
2025-07-09 22:09:22 +0800

    remove `n3` crate

--- crates/n3/src/quic/conn.rs
+++
@@ -1,9 +0,0 @@
-use quiche::ConnectionId;
-
-/// Create an new random [`ConnectionId`]
-pub fn random_conn_id() -> ConnectionId<'static> {
-    let mut buf = vec![0; 20];
-    boring::rand::rand_bytes(&mut buf).unwrap();
-
-    ConnectionId::from_vec(buf)
-}
--- crates/n3/src/quic/mod.rs
+++
@@ -1,7 +0,0 @@
-//! utilities for quic protocol.
-
-mod conn;
-pub use conn::*;
-
-mod validator;
-pub use validator::*;

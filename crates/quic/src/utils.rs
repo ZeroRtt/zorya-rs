b104894ffd595c5067a4bf69f8282c70e03fed4b
yayanyang <yayanyang@gmail.com>
2025-07-22 10:57:33 +0800

    quic: remove unused type `QuicSocket`

--- crates/quic/src/utils.rs
+++ crates/quic/src/utils.rs
@@ -7,14 +7,3 @@ pub fn random_conn_id() -> ConnectionId<'static> {
 
     ConnectionId::from_vec(buf)
 }
-
-/// Socket for quic.
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
-pub enum QuicSocket {
-    /// Socket for quic incoming listener.
-    Listener(usize),
-    /// Socket for quic connection.
-    Connection(usize),
-    /// Socket for quic stream.
-    Stream { conn_id: usize, stream_id: usize },
-}
